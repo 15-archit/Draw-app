2377780137926cfdb329c6a20b361b3279680723
Archit Vats <code.15archit@gmail.com>
2026-06-27 01:39:17 +0530

    init

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,13 @@
-import WebSocket,{WebSocketServer}  from "ws";
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    ws.send('pong'); 
+  });
+
+  ws.send('something');
+});
